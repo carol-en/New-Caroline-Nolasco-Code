313354a1c007e333a6a08b8104d14ce7e8429eb0
carol-en <cnolascoix@gmail.com>
2020-10-01 15:18:39 -0700

    wip sass variables

--- src/App.vue
+++ src/App.vue
@@ -17,12 +17,7 @@ export default {
 
 <style lang="scss">
 #app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   //this is where we use the variable
   color: $primary;
-  margin-top: 60px;
 }
 </style>
