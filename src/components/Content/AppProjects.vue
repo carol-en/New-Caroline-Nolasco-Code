01f8e9004dccdd8b971964d62dfe9b4a7e3806e0
Caroline Nolasco <carolinenolasco@Carolines-iMac.home>
2020-10-07 14:17:54 -0700

    even odd basic style to projects done

--- src/components/Content/AppProjects.vue
+++ src/components/Content/AppProjects.vue
@@ -32,6 +32,48 @@
               </figure>
           </div>
       </aside>
+
+      <aside class="prj">
+          <div class="prj-data">
+              <p>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Modi iste eos ad illum, distinctio recusandae facilis suscipit. Aliquid voluptas veniam dignissimos iusto ipsam sit magnam incidunt esse quasi, nemo voluptatibus?Architecto tenetur temporibus dicta labore sit molestias cum deleniti velit autem iusto voluptates quidem ipsa hic natus molestiae error tempore a, at enim perspiciatis. Quo necessitatibus repellendus modi beatae saepe.
+            </p>
+          </div>
+
+          <div class="prj-thumbnil">
+              <figure>
+                  <img src="https://i.imgur.com/K3BflwQ.png" alt="dummy data">
+              </figure>
+          </div>
+        </aside>
+
+      <aside class="prj">
+          <div class="prj-data">
+              <p>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Modi iste eos ad illum, distinctio recusandae facilis suscipit. Aliquid voluptas veniam dignissimos iusto ipsam sit magnam incidunt esse quasi, nemo voluptatibus?Architecto tenetur temporibus dicta labore sit molestias cum deleniti velit autem iusto voluptates quidem ipsa hic natus molestiae error tempore a, at enim perspiciatis. Quo necessitatibus repellendus modi beatae saepe.
+            </p>
+          </div>
+
+          <div class="prj-thumbnil">
+              <figure>
+                  <img src="https://i.imgur.com/K3BflwQ.png" alt="dummy data">
+              </figure>
+          </div>          
+        </aside>
+
+      <aside class="prj">
+          <div class="prj-data">
+              <p>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Modi iste eos ad illum, distinctio recusandae facilis suscipit. Aliquid voluptas veniam dignissimos iusto ipsam sit magnam incidunt esse quasi, nemo voluptatibus?Architecto tenetur temporibus dicta labore sit molestias cum deleniti velit autem iusto voluptates quidem ipsa hic natus molestiae error tempore a, at enim perspiciatis. Quo necessitatibus repellendus modi beatae saepe.
+            </p>
+          </div>
+
+          <div class="prj-thumbnil">
+              <figure>
+                  <img src="https://i.imgur.com/K3BflwQ.png" alt="dummy data">
+              </figure>
+          </div>
+        </aside>
   </section>
 
   
@@ -60,10 +102,18 @@ export default {
     .prj:nth-child(even) {
         background: $green-bg;
         color: $accent-font-color;
+
+        .prj-data {
+            order: 2;
+        }
     }
 
         .prj:nth-child(odd) {
         background: $light-bg;
         color: $primary-font-color;
+
+        .prj-thumbnil {
+            order: 1;
+        }
     }
 </style>
