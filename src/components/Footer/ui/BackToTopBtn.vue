9aec0f7fed4ded33af168e1fe3e4f48f32b2fc39
Caroline Nolasco <carolinenolasco@Carolines-iMac.home>
2021-01-12 11:53:34 -0800

    back to top button and methods complete

--- src/components/Footer/ui/BackToTopBtn.vue
+++ src/components/Footer/ui/BackToTopBtn.vue
@@ -1,8 +1,10 @@
 <template>
   <Fragment>
+      <transition name="fading">
       <button v-on:click="backToTop" v-show="scrolledDown" class="back-to-top">
-          Back To Top
+          <i class="fas fa-arrow-up"></i>
       </button>
+      </transition>
   </Fragment>
 
 </template>
@@ -20,41 +22,55 @@ export default {
     },
 
     mounted() {
-    this.trackScroll();
+        this.trackScroll();
+    },
+
+    destroyed() {
+        this.trackScroll();
     },
 
     methods: {
-        backToTop: function(e) {
+        backToTop: function(e) { // Jump bck to top of page after clicked
             e.preventDefault();
             scrollTo(0,0);    
-            
-            console.log('Back To Top Clicked');
         },
 
         trackScroll: function() {
-            window.onscroll =  function() {
-            // let windowHeight = window.innerHeight;
-            let scrollHeight = document.documentElement.scrollTop || document.body.scrollTop;
+            window.onscroll =  () => {
+                let windowHeight = window.innerHeight / 4;
+                let scrollHeight = document.documentElement.scrollTop || document.body.scrollTop;
 
-            console.log(this.scrolledDown, scrollHeight);
-
+                if(scrollHeight <= windowHeight) { // If page is at top is at the top & less than half of browser height
+                    if(this.scrolledDown === true) this.scrolledDown = false; // button is vanish
+                }
+                else if(scrollHeight > windowHeight) { // If page isn't at top and further down than half the browser page height
+                    if(this.scrolledDown === false) this.scrolledDown = true; // button will appear
+                }                
             }
         }       
     }
 }
 </script>
 <style scoped lang="scss">
+    .fading-enter-active, .fading-leave-active {
+    transition: opacity .5s;
+    }
+    .fading-enter, .fading-leave-to /* .fade-leave-active below version 2.1.8 */ {
+    opacity: 0;
+    }
     .back-to-top {
         background: $pink-bg;
         color: $accent-font-color;
         cursor: pointer;
-        border: 1px solid $accent-font-color;
-        font-size: .5em;
-        width: 7em;
-        height: 7em;
+        border-radius: 51%; 
+        border: 0 solid $white-color;
+        font-size: 1em;
+        transition: all .09s ease-in-out;
+        width: 3em;
+        height: 3em;
         position: fixed;
-        bottom: 2em;
-        right: 2em;
+        bottom: 1em;
+        right: 1em;
         z-index: 6;
     }
 
@@ -63,7 +79,7 @@ export default {
   .back-to-top:focus, 
   .back-to-top:focus-visible  {
       background: $accent-font-color;
-      border: 1px solid $accent-font-color;
+      border: 0 solid $white-color;
       color:  $pink-bg;
       outline: none;
   }    
